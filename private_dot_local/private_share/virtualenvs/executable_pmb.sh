09357a2349d1b8aa78aba039e2337aa9f9bbca14
David Personette <dperson@gmail.com>
2024-01-03 01:25:42 -0500

    first commit

---
+++ private_dot_local/private_share/virtualenvs/executable_pmb.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S bash
+
+set -euxo pipefail
+
+venv="${HOME}/.local/share/virtualenvs/pmb"
+
+[[ -d "${venv}" ]] || uv venv "${venv}"
+
+if ! grep -q 'pmbootstrap shutdown' "${venv}/bin/activate"; then
+  sed -Ei '/^deactivate \(\)/a \
+    # clean up \
+    if [ ! "${1:-}" = "nondestructive" ] ; then \
+        pmbootstrap shutdown \
+        yes | pmbootstrap zap --all \
+        sudo rm -rf .local/var/pmbootstrap/* "$VIRTUAL_ENV/pmbootstrap" \
+    fi \
+' "${venv}/bin/activate"
+fi
+
+if ! grep -q 'pmaports' "${venv}/bin/activate"; then
+  sed -Ei '/^# Call hash/i \
+[ -e "$VIRTUAL_ENV/pmbootstrap" ] && rm -rf "$VIRTUAL_ENV/pmbootstrap" \
+git clone --depth=1 https://gitlab.postmarketos.org/postmarketOS/pmbootstrap "$VIRTUAL_ENV/pmbootstrap" \
+yes | \\ln -firs "$VIRTUAL_ENV/pmbootstrap/pmbootstrap.py" "$VIRTUAL_ENV/bin/pmbootstrap" \
+pmbootstrap init \
+' "${venv}/bin/activate"
+fi
