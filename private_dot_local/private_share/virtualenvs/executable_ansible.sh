16de3cf413dd5ce0bcf82f846b57215004ac617c
David Personette <dperson@gmail.com>
2024-01-03 01:25:42 -0500

    first commit

---
+++ private_dot_local/private_share/virtualenvs/executable_ansible.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S bash
+
+set -euxo pipefail
+
+venv="${HOME}/.local/share/virtualenvs/ansible"
+
+[[ -d "${venv}" ]] || python -m venv "${venv}"
+
+[[ "${VIRTUAL_ENV:-""}" ]] || source "${venv}/bin/activate"
+
+pip install --upgrade pip
+pip install --upgrade \
+      ansible-builder \
+      'ansible-core<2.17' \
+      ansible-lint \
+      ansible-navigator \
+      dnspython \
+      jinja-cli \
+      ldap3 \
+      pyOpenSSL \
+      PySocks \
+      python-dateutil \
+      requests
+ansible-galaxy collection install --upgrade \
+      ansible.posix \
+      community.general \
+      community.mysql \
+      logicmonitor.integration \
+      theforeman.foreman
